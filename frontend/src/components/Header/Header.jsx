c5dee21476d90fb4725694a6da0699b32cc21a28
Naseef Mashood <nosfeeferatu@gmail.com>
2024-07-18 11:08:41 +0530

    header bg

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef } from "react";
 import logo from "../../assets/images/logo.png";
+import userImg from "../../assets/images/avatar-icon.png";
 import { NavLink, Link } from "react-router-dom";
+import { BiMenu } from "react-icons/bi";
 
 const navLinks = [
   {
@@ -41,7 +43,7 @@ const Header = () => {
                     className={(navClass) =>
                       navClass.isActive
                         ? "text-primaryColor text-[16px] leading-7 font-[600]"
-                        : "text-textColor text-[16px] leading-7 font-[500]"
+                        : "text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor"
                     }
                   >
                     {link.display}
@@ -49,6 +51,27 @@ const Header = () => {
                 </li>
               ))}
             </ul>
+          </div>
+
+          {/* NAV RIGHT */}
+          <div className="flex items-center gap-4">
+            <div>
+              <Link to="/">
+                <figure className="w-[35px] h-[35px] rounded-full cursor-pointer">
+                  <img src={userImg} className="w-full rounded-full" alt="" />
+                </figure>
+              </Link>
+            </div>
+
+            <Link to="/login">
+              <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]">
+                Login
+              </button>
+            </Link>
+
+            <span className="md:hidden">
+              <BiMenu className="w-6 h-6 cursor-pointer" />
+            </span>
           </div>
         </div>
       </div>
