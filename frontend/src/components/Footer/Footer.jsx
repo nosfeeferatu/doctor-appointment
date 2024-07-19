63576d736a39c7df125d580a11925f81ddee93ca
gavinlivero <gavinlivero@gmail.com>
2024-07-19 11:28:53 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,4 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from '../../assets/images/logo.png';
+import {RiLinkedinFill} from 'react-icons/ri';
+import {AiFillYoutube, AiFillGithub, AiOutlineInstagram} from 'react-icons/ri';
+
+
+const socialLinks = [
+  {
+    path: "https://www.youtube.com/c/CodingWithMuhib",
+    icon: <AiFillYoutube className="group-hover: text-white w-4 h-5"/>
+  },
+  {
+    path: "https://github.com/codingwithmuhib",
+    icon: <AiFillGithub className="group-hover: text-white w-4 h-5"/>
+  },
+  {
+    path: "https://www.instagram.com/muhiib168.official/",
+    icon: <AiOutlineInstagram className="group-hover: text-white w-4 h-5"/>
+  }
+]
+
 
 const Footer = () => {
   return <div>Footer</div>;
