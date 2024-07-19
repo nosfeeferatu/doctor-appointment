a87e56f1b04ff559a3b22dc460f4f3df82aec076
Naseef Mashood <nosfeeferatu@gmail.com>
2024-07-19 11:25:53 +0530

    About done

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import aboutImg from "../../assets/images/about.png";
+import aboutCardImg from "../../assets/images/about-card.png";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
@@ -9,6 +11,31 @@ const About = () => {
           {/* ABOUT IMG */}
           <div className="relative w-3/4 lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1">
             <img src={aboutImg} alt="" />
+            <div className="absolute z-20 bottom-4 w-[200px] md:w-[300px] right-[-30%] md:right-[-7%] lg:right-[22%]">
+              <img src={aboutCardImg} alt="" />
+            </div>
+          </div>
+
+          {/* ABOUT CONTENT */}
+          <div className="w-full lg:w-1/2 xl:w-[670px] order-1 lg:order-2">
+            <h2 className="heading">Proud to be one of the nations best</h2>
+            <p className="text__para">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas
+              pariatur atque, corporis, doloremque, dolore consequatur doloribus
+              voluptatibus quas expedita neque vero magnam! Omnis consequuntur
+              iste numquam quo cupiditate sunt saepe eos impedit quos? Labore,
+              recusandae nobis! Neque repellendus nemo, itaque molestias soluta
+              omnis repudiandae culpa, sequi sed dolorem ad nostrum.
+            </p>
+
+            <p className="text__para mt-[30px]">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis
+              reprehenderit dolorem dolore magnam ratione voluptatum!
+            </p>
+
+            <Link to="/">
+              <button className="btn">Learn More</button>
+            </Link>
           </div>
         </div>
       </div>
