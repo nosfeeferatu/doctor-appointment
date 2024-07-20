1e5aa80f674e2a568d2ccc8b188ddfa2528d24b7
Naseef Mashood <nosfeeferatu@gmail.com>
2024-07-20 23:09:58 +0530

    registration backend

---
+++ backend/Controllers/authController.js
@@ -0,0 +1,62 @@
+import User from "../models/UserSchema.js";
+import Doctor from "../models/DoctorSchema.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+export const register = async (req, res) => {
+  const { name, email, password, role, gender, photo } = req.body;
+
+  try {
+    let user = null;
+    if (role === "patient") {
+      user = await User.findOne({ email });
+    } else if (role === "doctor") {
+      user = await Doctor.findOne({ email });
+    }
+
+    // Check if existing user
+    if (user) {
+      return res.status(400).json({ message: "User already exists." });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    if (role === "patient") {
+      user = new User({
+        name,
+        email,
+        password: hashPassword,
+        role,
+        gender,
+        photo,
+      });
+    }
+
+    if (role === "doctor") {
+      user = new Doctor({
+        name,
+        email,
+        password: hashPassword,
+        role,
+        gender,
+        photo,
+      });
+    }
+
+    await user.save();
+    res
+      .status(200)
+      .json({ success: true, message: "User created successfully" });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, message: "Internal server error, try again" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+  } catch (err) {}
+};
