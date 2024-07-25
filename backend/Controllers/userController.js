a9a0b8016f5fa4db42eb88e0d5ffa96433c2f627
Naseef Mashood <nosfeeferatu@gmail.com>
2024-07-25 11:13:30 +0530

    update and delete user functions

---
+++ backend/Controllers/userController.js
@@ -0,0 +1,36 @@
+import User from "../models/UserSchema.js";
+
+export const updateUser = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { $set: req.body },
+      { new: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      data: updatedUser,
+    });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Failed to update user" });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    await User.findByIdAndDelete(id);
+
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully",
+    });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Failed to delete user" });
+  }
+};
