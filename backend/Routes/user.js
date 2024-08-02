ab8b5ee0ed486d9e78da53b8ae8087346b2807b5
Naseef Mashood <nosfeeferatu@gmail.com>
2024-08-03 00:43:29 +0530

    authentication token

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -6,10 +6,12 @@ import {
   getAllUsers,
 } from "../Controllers/userController.js";
 
+import { authenticate } from "../auth/verifyToken.js";
+
 const router = express.Router();
 
 router.get("/", getAllUsers);
-router.get("/:id", getSingleUser);
+router.get("/:id", authenticate, getSingleUser);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 
