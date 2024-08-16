633fb8d0edad2180b3190b5f9b8aad4d4a85bfd0
leantos <84658038+leantos@users.noreply.github.com>
2024-08-16 11:34:31 +0530

    Update DoctorAbout.jsx

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -30,7 +30,7 @@ const DoctorAbout = () => {
           <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
             <div>
               <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
-                {formatDate("07-10-2014")}
+                {formatDate("07-10-2014")} - {formatDate("09-10-2015")}
               </span>
               <p className="text-[16px] leading-6 font-medium text-textColor">
                 Post Doc. in Rapid Surgery Techniques
@@ -43,12 +43,43 @@ const DoctorAbout = () => {
           <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
             <div>
               <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
-                {formatDate("04-04-2010")}
+                {formatDate("04-04-2010")} - {formatDate("07-11-2011")}
               </span>
               <p className="text-[16px] leading-6 font-medium text-textColor">
                 Post Doc. in Rapid Surgery Techniques
               </p>
             </div>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              His Higness Kim Jong-Il Institute of Medicine,Pyongyang.
+            </p>
+          </li>
+        </ul>
+      </div>
+
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+          Experience
+        </h3>
+
+        <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColortext-[15px] leading-6 font-semibold">
+              {formatDate("07-10-2010")} - {formatDate("09-10-2015")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Sr. Surgeon
+            </p>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              His Higness Kim Jong-Il Institute of Medicine,Pyongyang.
+            </p>
+          </li>
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColortext-[15px] leading-6 font-semibold">
+              {formatDate("07-10-2010")} - {formatDate("09-10-2015")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Jr. Surgeon
+            </p>
             <p className="text-[14px] leading-5 font-medium text-textColor">
               His Higness Kim Jong-Il Institute of Medicine,Pyongyang.
             </p>
