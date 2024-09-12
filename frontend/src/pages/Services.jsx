619dce681881a53156c229f23ce8b1a6ad0b5da9
Naseef Mashood <nosfeeferatu@gmail.com>
2024-09-13 00:41:48 +0530

    Services in progress

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -4,7 +4,7 @@ import ServiceCard from "../components/Services/ServiceCard";
 const Services = () => {
   return (
     <div className="container">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] mb-[80px] lg:mt-[55px] mb-[80px]">
         {services.map((item, index) => (
           <ServiceCard item={item} index={index} key={index} />
         ))}
