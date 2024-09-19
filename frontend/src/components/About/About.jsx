2249b900a891436b27523c067d7bd439d2330f0b
Naseef Mashood <nosfeeferatu@gmail.com>
2024-09-19 11:18:03 +0530

    Doctor card, images update

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -7,13 +7,10 @@ const About = () => {
   return (
     <section>
       <div className="container">
-        <div className="flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row">
+        <div className="flex justify-between gap-[50px] lg:gap-[130px] flex-col lg:flex-row">
           {/* ABOUT IMG */}
-          <div className="relative w-3/4 lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1">
+          <div className="relative w-1/2 z-10 order-2 lg:order-1">
             <img src={aboutImg} alt="" />
-            <div className="absolute z-20 bottom-4 w-[200px] md:w-[300px] right-[-30%] md:right-[-7%] lg:right-[22%]">
-              <img src={aboutCardImg} alt="" />
-            </div>
           </div>
 
           {/* ABOUT CONTENT */}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -206,31 +206,6 @@ const Home = () => {
             </div>
             <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
               <img src={featureImg} alt="" className="w-3/4" />
-              <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]">
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center gap-[6px] lg:gap-3">
-                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]">
-                      Tue, 24
-                    </p>
-                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[400]">
-                      10:00AM
-                    </p>
-                  </div>
-                  <span className="w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center jusitfy-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]">
-                    <img src={videoIcon} alt="" />
-                  </span>
-                </div>
-
-                <div className="w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:py-[6px] lg:px-[10px] text-[8px] lg:text-[12px] lg:leading-[8px] lg:text-[12px] lg;leading-4 text-irisblueColor font-[500] mt-2 lg:mt-4 rounded-full">
-                  Consultation
-                </div>
-                <div className="flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]">
-                  <img src={avatarIcon} alt="" />
-                  <h4 className="text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor">
-                    Wayne Collins
-                  </h4>
-                </div>
-              </div>
             </div>
           </div>
         </div>
