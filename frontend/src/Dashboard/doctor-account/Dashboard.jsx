762b641caab939cd44be29bd9a2011ab46df1305
leantos <84658038+leantos@users.noreply.github.com>
2024-09-20 11:38:44 +0530

    Started Tabs
    https://youtu.be/Jz4vQKF0mTk?si=N77THEmlG8RB3BUn&t=738

--- frontend/src/Dashboard/doctor-account/Dashboard.jsx
+++ frontend/src/Dashboard/doctor-account/Dashboard.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import Loader from "../../components/Loader/Loading";
+import Error from "../../components/Error/Error";
+import useGetProfile from "../../hooks/useFetchData";
+import { BASE_URL } from "../../config";
+import Tabs from "./Tabs";
+import { useState } from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { data, loading, error } = useGetProfile(
+    `${BASE_URL}/doctors/profile/me`
+  );
+
+  const [tab, setTab] = useState("overview");
+
+  return (
+    <section>
+      <div className="max-w-[1170px] px-5 mx-auto">
+        {loading && !error && <Loader />}
+        {error && !loading && <Error />}
+
+        {!loading && (
+          <div className="grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]">
+            <Tabs tab={tab} setTab={setTab} />
+            <div className="lg:col-span-2">
+              {data.isApproved === "pending" && (
+                <div className="flex p-4 mb-4 text-yellow-800 bg-yellow-50 rounded-lg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    x="0px"
+                    y="0px"
+                    width="100"
+                    height="100"
+                    viewBox="0 0 50 50"
+                  >
+                    <path d="M 25 2 C 12.309295 2 2 12.309295 2 25 C 2 37.690705 12.309295 48 25 48 C 37.690705 48 48 37.690705 48 25 C 48 12.309295 37.690705 2 25 2 z M 25 4 C 36.609824 4 46 13.390176 46 25 C 46 36.609824 36.609824 46 25 46 C 13.390176 46 4 36.609824 4 25 C 4 13.390176 13.390176 4 25 4 z M 25 11 A 3 3 0 0 0 22 14 A 3 3 0 0 0 25 17 A 3 3 0 0 0 28 14 A 3 3 0 0 0 25 11 z M 21 21 L 21 23 L 22 23 L 23 23 L 23 36 L 22 36 L 21 36 L 21 38 L 22 38 L 23 38 L 27 38 L 28 38 L 29 38 L 29 36 L 28 36 L 27 36 L 27 21 L 26 21 L 22 21 L 21 21 z"></path>
+                  </svg>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Dashboard;
