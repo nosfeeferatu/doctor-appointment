d62e5ca755d1b78b64aa474e0bd0f272f49c7089
gavinlivero <gavinlivero@gmail.com>
2024-09-29 16:52:51 +0530

    reviewDeets,toastMessagesEtc

--- frontend/src/pages/Doctors/Feedback.jsx
+++ frontend/src/pages/Doctors/Feedback.jsx
@@ -5,40 +5,41 @@ import { formatDate } from "../../utils/formatDate";
 import { useState } from "react";
 import FeedbackForm from "./FeedbackForm";
 
-const Feedback = () => {
+const Feedback = ({reviews, totalRating}) => {
   const [showFeedbackForm, setShowFeedbackForm] = useState(false);
   return (
     <div>
       <div className="mb-[50px]">
         <h4 className="text-[20px] leading-[30px] font-bold text-headingColor mb-[30px]">
-          All reviews (1)
+          All reviews ({totalRating})
         </h4>
-        <div className="flex justify-between gap-10 mb-[30px]">
+        { reviews?.map((review,index)=> (
+          <div key={index} className="flex justify-between gap-10 mb-[30px]">
           <div className="flex gap-3">
             <figure className="w-10 h-10 rounded-full">
-              <img className="w-full" src={avatar} alt="" />
+              <img className="w-full" src={review?.user?.photo} alt="" />
             </figure>
 
             <div>
               <h5 className="text-[16px] leading-6 text-primaryColor font-bold">
-                Mohammed Naseef bin Mashood
+                {review?.user?.name}
               </h5>
               <p className="text-[14px] leading-6 text-textColor">
-                {formatDate("08-16-2024")}
+                {formatDate(review?.createdAt)}
               </p>
               <p className="text__para mt-3 font-medium text-[15px]">
-                Punched me after treatment, now I&apos;m back in hospital 10/10
-                experience
+                {review.reviewText}
               </p>
             </div>
           </div>
 
           <div className="flex gap-1">
-            {[...Array(5).keys()].map((_, index) => (
+            {[...Array(review?.rating).keys()].map((_, index) => (
               <AiFillStar key={index} color="#0067FF" />
             ))}
           </div>
         </div>
+        ))}
       </div>
       {!showFeedbackForm && (
         <div className="text-center">
