0a3a7dd0c7396c41901275cbbe2280953152e56b
Naseef Mashood <nosfeeferatu@gmail.com>
2024-09-30 19:27:00 +0530

    Services delete function

--- frontend/src/Dashboard/admin-account/DisplayServices.jsx
+++ frontend/src/Dashboard/admin-account/DisplayServices.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import { useState } from "react";
 
 import useFetchData from "../../hooks/useFetchData";
-import { AiOutlineEye } from "react-icons/ai";
-import { BASE_URL } from "../../config";
+import { AiOutlineDelete } from "react-icons/ai";
+import { BASE_URL, token } from "../../config";
 import { DataGrid } from "@mui/x-data-grid";
 import Loading from "../../components/Loader/Loading";
 import Error from "../../components/Error/Error";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import SyncLoader from "react-spinners/SyncLoader";
 
 const DisplayServices = () => {
   const {
@@ -13,6 +24,43 @@ const DisplayServices = () => {
     loading,
     error,
   } = useFetchData(`${BASE_URL}/services`);
+
+  const [open, setOpen] = useState(false);
+  const [loader, setLoader] = useState(false);
+  const [deleteID, setDeleteID] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const deleteServiceItem = async (id) => {
+    try {
+      setLoader(true);
+      const res = await fetch(`${BASE_URL}/services/delete/${id}`, {
+        method: "DELETE",
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log(`${BASE_URL}/services/delete/${id}`);
+
+      const result = await res.json();
+      if (!res.ok) {
+        throw Error(result.message);
+      }
+      setLoader(false);
+      toast.success(result.message);
+      setOpen(false);
+    } catch (err) {
+      toast.error(err.message);
+      setLoader(false);
+      setOpen(false);
+    }
+    navigate(0);
+  };
 
   const columns = [
     {
@@ -36,9 +84,15 @@ const DisplayServices = () => {
     {
       headerName: "Actions",
       renderCell: ({ row }) => (
-        <div className=" p-4 rounded-full text-[18px] cursor-pointer">
-          <AiOutlineEye />
-        </div>
+        <button
+          onClick={() => {
+            setDeleteID(row._id);
+            setOpen(true);
+          }}
+          className=" p-4 rounded-full text-[18px] cursor-pointer"
+        >
+          <AiOutlineDelete />
+        </button>
       ),
     },
   ];
@@ -47,13 +101,46 @@ const DisplayServices = () => {
       {loading && <Loading />}
       {error && <Error errMessage={error} />}
       {!loading && !error && (
-        <DataGrid
-          autoHeight
-          columns={columns}
-          rows={services}
-          getRowId={(row) => row._id}
-          sx={{ "--DataGrid-overlayHeight": "300px", background: "" }}
-        />
+        <>
+          <DataGrid
+            autoHeight
+            columns={columns}
+            rows={services}
+            getRowId={(row) => row._id}
+            sx={{ "--DataGrid-overlayHeight": "300px", background: "" }}
+          />
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">{"Are you sure?"}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                This data, once deleted, cannot be recovered. Are you sure you
+                want to delete this?
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <button
+                disabled={loader}
+                className=" text-primaryColor bg-white hover:bg-lighterColor text-[18px] leading-8 rounded-lg px-4 py-1"
+                onClick={() => deleteServiceItem(deleteID)}
+              >
+                {loader ? <SyncLoader size={10} color="#ffffff" /> : "Yes"}
+              </button>
+              <button
+                className=" bg-primaryColor text-white hover:bg-lighterColor hover:text-primaryColor text-[18px] leading-8 rounded-lg px-4 py-1"
+                onClick={handleClose}
+                autoFocus
+                disabled={loader}
+              >
+                No
+              </button>
+            </DialogActions>
+          </Dialog>
+        </>
       )}
     </>
   );
