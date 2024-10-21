3ef38e79569f7dfc1a82a313d9d108e4d3c08c13
Naseef Mashood <nosfeeferatu@gmail.com>
2024-10-22 03:00:19 +0530

    upload photo text width fix

--- frontend/src/pages/DoctorSignup.jsx
+++ frontend/src/pages/DoctorSignup.jsx
@@ -168,7 +168,7 @@ const DoctorSignup = () => {
 
                   <label
                     htmlFor="customFile"
-                    className="absolute top-0 left-0 w-full h-full flex items-center px-[0.75rem] py-[0.375rem] text-[15px] leading-6 overflow-hidden bg-[#0066ff46] text-headingColor font-semibold rounded-lg truncate cursor-pointer"
+                    className="absolute top-0 left-0 h-full flex items-center px-[0.75rem] py-[0.375rem] text-[15px] leading-6 overflow-hidden bg-[#0066ff46] text-headingColor font-semibold rounded-lg truncate cursor-pointer"
                   >
                     Upload Photo
                   </label>
